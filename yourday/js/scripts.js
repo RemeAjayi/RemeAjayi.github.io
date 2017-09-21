60562caf0e0ed27f9889b7b82ceb73926dea67f9
Olohireme <remeajayi@gmail.com>
2017-09-21 07:57:01 +0100

    first commit

--- yourday/js/scripts.js
+++ yourday/js/scripts.js
@@ -1,22 +1,14 @@
 $('document').ready(function()
 { 
-   $('.form').keyup(function(){
+   $('.required').keyup(function(){
   
    if (($('input[name="name"]') && $('input[name="q3"]') && $('input[name="q2"]') !== "") && $('input[name="q1"]').is(':checked') ) 
    {
     $('#submitButton').removeAttr('disabled');
    }
 
-});
-    // just an alert
-    $('#submit').click(function(){
-    alert("Thanks for Sharing!");
-    });
 
 });
-//create a fxn to enable button using serialize
-/*
-
    //displays error message
    $('.required').blur(function(){
     var value = $(this).val();
@@ -28,6 +20,11 @@ $('document').ready(function()
       $(this).css({"color": "red", "border": "2px solid red"});
       
     }
-   hide to remove and change css to black and 0
-   });
-   */
+    // just an alert
+    $('#submit').click(function(){
+    alert("Thanks for Sharing!");
+    });
+
+});
+//hide to remove and change css to black and 0
+  
